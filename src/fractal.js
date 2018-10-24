92297a94e0e417e9cd2efa9ddd33125c32f222d6
Robert Collins <beau@collins.pub>
2018-10-24 16:09:21 -0700

    OMG it works

--- src/fractal.js
+++ src/fractal.js
@@ -5,3 +5,57 @@
 export function add<T: (string | number)>(a: T, b: T): T {
     return a + b;
 }
+
+export type Reducer<S, A> = (S | void, A) => S;
+
+export type Selector<S, O, R> = (S, O) => R;
+type ActionCreator<A, O> = (O) => A;
+
+type MapSelector<X> = <S, O, R>(Selector<S, O, R>) => Selector<X, O, R>;
+type MapAction<X> = <O, A>(ActionCreator<A, O>) => ActionCreator<X, O>;
+
+export type Fractal<S, A, X, E, SE: SelectorSet<S>, AE: ActionSet<A>> = {|
+    selectors: $ObjMap<SE, MapSelector<X>>,
+    actions: $ObjMap<AE, MapAction<E>>,
+    reducer: Reducer<S, E>,
+    select: (X) => S
+|}
+
+type Integration<S, A, X, E> = {|
+    getState: (X) => S,
+    detectAction: (E) => ?A,
+    createAction: (A) => E,
+|};
+
+type SelectorSet<T> = {
+    [selector: string]: Selector<T, *, *>
+};
+
+type ActionSet<A> = {
+    [action: string]: ActionCreator<A, *>
+};
+
+export function createFractal<S, A, X, E, SE: SelectorSet<S>, AE: ActionSet<A>>(
+    defaultState: S,
+    reducer: Reducer<S, A>,
+    selectors: SE,
+    actions: AE,
+    api: Integration<S, A, X, E>,
+): Fractal<S, A, X, E, SE, AE> {
+    return {
+        selectors: Object.keys(selectors).reduce((mapped, key) => Object.assign(mapped, {
+            [key]: (state, options) => selectors[key](api.getState(state), options)
+        }), {}),
+        actions: Object.keys(actions).reduce((mapped, key) => Object.assign(mapped, {
+            [key]: (options) => api.createAction(actions[key](options))
+        }), {}),
+        select: api.getState,
+        reducer: (state = defaultState, external) => {
+            const action = api.detectAction(external);
+            if (action) {
+                return reducer(state, action);
+            }
+            return state;
+        },
+    };
+}
--- test/fractal.test.js
+++ test/fractal.test.js
@@ -1,8 +1,89 @@
 /**
  * @flow strict
  */
-import { add } from 'fractal';
+import { add, createFractal } from 'fractal';
+import type { Fractal, Reducer } from 'fractal';
 
 test('thing', () => {
     expect(add(1, 2)).toBe(3);
+});
+
+type State = 'a' | 'b';
+type Action = 'one' | 'two';
+const reducer: Reducer<State, Action> = (state, action) => {
+    switch(action) {
+    case 'one': {
+        return 'a';
+    }
+    case 'two': {
+        return 'b';
+    }
+    default: {
+        (action: empty);
+        throw new Error('not handled');
+    }
+    }
+};
+
+test('pure fractal', () => {
+    const selectors = {
+        all: (s: State) => s,
+    };
+    const actions = {
+        one: () => 'one',
+        two: () => 'two',
+    };
+    const fractal: Fractal<State, Action, State, Action, typeof selectors, *> = createFractal('a', reducer, selectors, actions, {
+        getState: state => state,
+        createAction: internal => internal,
+        detectAction: external => external
+    });
+    expect(fractal.reducer(undefined, 'one')).toBe('a');
+    expect(fractal.reducer(undefined, 'two')).toBe('b');
+    expect(fractal.select('b')).toBe('b');
+    expect(fractal.selectors.all('b')).toBe('b');
+
+    expect(fractal.actions.one()).toBe('one');
+    expect(fractal.actions.two()).toBe('two');
+});
+
+type ExternalAction = 
+    | { type: 'external', action: Action }
+    | { type: 'other' }
+type ExternalState = {
+    internal: State,
+}
+
+test('mapped fractal', () => {
+
+    const selectors = {
+        all: (thing: State) => thing,
+        isA: (thing: State): boolean => thing === 'a',
+        blerg: (state: State, is: string) => state === is,
+    };
+
+    const actions = {
+        one: () => 'one',
+        two: () => 'two',
+        item: (action: string) => 'one',
+    };
+
+    const fractal = createFractal('a', reducer, selectors, actions, {
+        getState: (state: ExternalState): State => state.internal,
+        detectAction: (external: ExternalAction): ?Action => external.type === 'external' ? external.action : null,
+        createAction: action => ({ type: 'external', action })
+    });
+
+    expect(fractal.reducer(undefined, { type: 'external', action: 'one'})).toBe('a');
+    expect(fractal.reducer(undefined, { type: 'external', action: 'two'})).toBe('b');
+    expect(fractal.select({ internal: 'b' })).toBe('b');
+    expect(fractal.selectors.all({ internal: 'b' })).toBe('b');
+    expect(fractal.selectors.isA({ internal: 'b'})).toBe(false);
+    expect(fractal.selectors.isA({ internal: 'a'})).toBe(true);
+    expect(fractal.selectors.blerg({ internal: 'a' }, 'a')).toBe(true);
+
+    expect(fractal.actions.one()).toEqual({ action: 'one', type: 'external' });
+    expect(fractal.actions.two()).toEqual({ action: 'two', type: 'external' });
+    expect(fractal.actions.item('a')).toEqual({ action: 'a', type: 'external' });
+
 });
